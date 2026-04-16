5f71109176b3285437af9495a99cb8b5a23a6cbd
httpsJadson <dev.jadson27@gmail.com>
2026-04-16 10:38:03 -0300

    feat: add category moto to category enum, add search per category, model, year, brand and add errors tratament

--- back/src/vehicles/vehicle.category.enum.ts
+++ back/src/vehicles/vehicle.category.enum.ts
@@ -1,20 +1,10 @@
 export enum Category {
-  /**
-  enum Category {
-    HATCH
-    SEDAN
-    SUV
-    PICKUP
-    COUPE
-    VAN
-    ELECTRIC
-  }
-   */
   HATCH = 'HATCH',
   SEDAN = 'SEDAN',
   SUV = 'SUV',
   PICKUP = 'PICKUP',
   COUPE = 'COUPE',
   VAN = 'VAN',
-  ELECTRIC = 'ELECTRIC'
+  ELECTRIC = 'ELECTRIC',
+  MOTO = 'MOTO'
 }
