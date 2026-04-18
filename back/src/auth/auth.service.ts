cfd39468dff896caa82000f98c3e4fc774fce505
httpsJadson <dev.jadson27@gmail.com>
2026-04-18 14:13:19 -0300

    feat: add auth module and encryptation password module

--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -1,26 +1,65 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { BadRequestException, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { PrismaService } from '../database/prisma.service';
+import { HashingServiceProtocol } from './hashing/hashing.service';
+import jwtConfig from './config/jwt.config';
+import { JwtService } from '@nestjs/jwt/dist/jwt.service';
+
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly hashingService: HashingServiceProtocol,
+
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ReturnType<typeof jwtConfig>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: LoginDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: loginDto.email,
+      },
+    });
+    // console.log(user)
+
+    if (user) {
+      const passwordIsValid = await this.hashingService.compare(
+        loginDto.password,
+        user.password,
+      );
+      passwordIsValid ? true : this.unauthorized();
+      const accessToken = await this.jwtService.signAsync(
+        {
+          sub: user.id,
+          email: user.email,
+          role: user.jobRole,
+        },
+        {
+          audience: this.jwtConfiguration.audi,
+          issuer: this.jwtConfiguration.issuer,
+          secret: this.jwtConfiguration.secret,
+          expiresIn: this.jwtConfiguration.jwtTTL,
+        }
+      );
+      // console.log(accessToken)
+      return {  
+        access_token: accessToken,
+        user: {
+          id: user.id,
+          email: user.email,
+          name: user.username,
+          role: user.jobRole,
+        }
+      }
+    } else {
+      this.unauthorized();
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  private readonly unauthorized = () => {
+    throw new UnauthorizedException("Invalid email or password");
   }
 }
