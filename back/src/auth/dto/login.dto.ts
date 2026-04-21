27cb026dae417f4de8a66e5f5201cfd2fc7c8e23
httpsJadson <dev.jadson27@gmail.com>
2026-04-21 18:44:15 -0300

    feat: add function to refreshe tokens JWT

--- back/src/auth/dto/login.dto.ts
+++ back/src/auth/dto/login.dto.ts
@@ -1,10 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger/dist/decorators/api-property.decorator";
 import { IsEmail, IsEmpty, IsNotEmpty, IsString } from "class-validator";
 
 export class LoginDto {
 
+  @ApiProperty({ description: 'User email' })
+  @IsEmail()  
   @IsEmail()  
   email: string;
 
+  @ApiProperty({ description: 'User password' })
   @IsString()
   @IsNotEmpty()
   password: string;
