b4075a721ac92cf0dbcb31c875103ceb497a32dd
httpsJadson <dev.jadson27@gmail.com>
2026-04-22 21:46:57 -0300

    feat: add types mapping interface fot the contrato with api

--- front/src/types/vehicles.ts
+++ front/src/types/vehicles.ts
@@ -2,3 +2,19 @@ export interface Vehicle {
   id: number;
   fullName: string;
 }
+
+export interface VehicleCreate {
+
+    brand: string,
+    model: string,
+    version: string,
+    year: number,
+    category: string,
+    consumptionCityG?: number,
+    consumptionHwyG?: number,
+    consumptionCityE?: number,
+    consumptionHwyE?: number,
+    consumptionHwyD?: number,
+    consumptionCityD?: number,
+
+}
