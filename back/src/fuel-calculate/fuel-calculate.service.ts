dee4a9cfe7510bd659d9c1ef4127630b91c4ee39
viniciusvnb17 <viniciusbarrosvnb@gmail.com>
2026-04-24 11:51:03 -0300

    vinicius/ajustes finais antes de mudar

--- back/src/fuel-calculate/fuel-calculate.service.ts
+++ back/src/fuel-calculate/fuel-calculate.service.ts
@@ -9,9 +9,9 @@ export class FuelCalculateService {
 
   async realizarTesteMockado() {
     // Dados para o cálculo
-    const distancia = 18; 
-    const precos = { gasolina: 5.80, etanol: 3.90, diesel: 6.00 };
-    const medias = { gasolina: 40, etanol: 7, diesel: 12 };
+    const distancia = 318; 
+    const precos = { gasolina: 6, etanol: 3.90, diesel: 6.00 };
+    const medias = { gasolina: 11, etanol: 7, diesel: 12 };
 
     // Chama a função do calc.ts
     const resultado = calcularConsumoEReais(distancia, precos, medias);
