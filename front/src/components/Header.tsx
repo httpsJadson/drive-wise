325d6794c7d57970685dd962a6351558285fe7cd
httpsJadson <dev.jadson27@gmail.com>
2026-04-26 20:10:16 -0300

    remove - in the title

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -9,7 +9,7 @@ export function Header() {
       <div className="container mx-auto px-6 md:px-12 flex justify-center md:justify-start items-center relative z-10">
         <div className="flex items-center group cursor-default select-none">
           <span className="text-white text-3xl font-extrabold tracking-tighter">
-            Drive - 
+            Drive
           </span>
           <span className="text-blue-100 text-3xl font-light tracking-tight ml-0.5">
             Wise
